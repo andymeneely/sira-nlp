0438ad4c7743b17a879ef372f2f0292644bef714
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-10-12 23:11:12 -0400

    Read database settings from an external file
     Having the database settings in a source-controlled file increases the
     likelihood of a developer accidentally committing the file with the
     passwords in it. The default database settings are still in the
     settings.py file, however, a separate file, dbsettings.py, has
     alternative database settings dictionary which replaces the one from
     settings.py. The file dbsettings.py is git-ignored so the risk of
     accidentally committing database password is considerably reduced. The
     dbsettings.py.template file is a template of the dbsettings.py file.

--- linsights/settings.py
+++ linsights/settings.py
@@ -1,15 +1,24 @@
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '#cmj@ekvxqjysua*+49&f-erlf$avg@$8hppv^8i=9g+86(j8a'
-DEBUG = False
-INSTALLED_APPS = ['app']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+try:
+    from linsights import dbsettings
+    DATABASES = dbsettings.get()
+except ImportError:
+    pass
+
+SECRET_KEY = '#cmj@ekvxqjysua*+49&f-erlf$avg@$8hppv^8i=9g+86(j8a'
+DEBUG = False
+INSTALLED_APPS = ['app']
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
