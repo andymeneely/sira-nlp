7f9a8fa95d30af66c8be57d9dea17b7a095d38f1
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-12-31 01:42:28 -0500

    Shorten message type tag

--- app/lib/logger.py
+++ app/lib/logger.py
@@ -6,16 +6,16 @@ from termcolor import cprint
 
 def debug(message):
     if 'DEBUG' in os.environ:
-        cprint('\r[DEBUG] {0}'.format(message), 'blue', file=sys.stderr)
+        cprint('\r[DBG] {0}'.format(message), 'blue', file=sys.stderr)
 
 
 def info(message):
-    cprint('\r[INFO] {0}'.format(message), 'white', file=sys.stdout)
+    cprint('\r[INF] {0}'.format(message), 'white', file=sys.stdout)
 
 
 def warning(message):
-    cprint('\r[WARNING] {0}'.format(message), 'yellow', file=sys.stderr)
+    cprint('\r[WRN] {0}'.format(message), 'yellow', file=sys.stderr)
 
 
 def error(message):
-    cprint('\r[ERROR] {0}'.format(message), 'red', file=sys.stderr)
+    cprint('\r[ERR] {0}'.format(message), 'red', file=sys.stderr)
