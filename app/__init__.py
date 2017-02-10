0296ce6d3adcc646b17061557d9080345cbafc59
Benjamin Meyers <bsm9339@rit.edu>
2017-02-09 19:27:04 -0500

    Added output file paths for TFIDF.

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+TFIDF_TOKENS_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'output/tfidf_tokens.csv'
+    )
+TFIDF_LEMMAS_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'output/tfidf_lemmas.csv'
+    )
+
