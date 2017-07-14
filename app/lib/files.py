andymeneely/sira-nlp

commit e9c70f9b7ffd1915ed144de4bd04946a26c8a723

Load bugs JSONs into the database
After a lot of waiting, the bugs from Monorail have been downloaded in
their JSON formats using the Monorail API. The change switches loading
the bugs from CSV files downloaded from the Monorail's web interface
with the JSON files. The test CSV files are being removed with the
addition of the JSON files.

In addition to the change described, few refactoring changes were
implemented chief among which is the addition of tests to test
additional functions in app/lib/files.py.

app/lib/files.py
@@ -25,19 +25,47 @@ class Files(object):
         self.vulnerabilities_path = settings.VULNERABILITIES_PATH
         self.bots = settings.BOTS
 
-    def get_bugs(self, year):
+    def get_bug(self, id, year=None):
+        """Retrieve a bug identified by the unique identifier specified.
+
+        Parameters
+        ----------
+        id : int
+            Unique identifier of the bug to retrieve.
+
+        Returns
+        -------
+        bug : dict
+            Bug identified by the identifier specified. An exception is raised
+            when no bug was found.
         """
-        Return a list of bugs that were associated with the specified year.
-        """
-        bugs = list()
+        year = self.get_year(id, switch='bugs') if year is None else year
         directory = self.get_bugs_path(year)
-        for path in self._get_files(directory, pattern='bugs.csv'):
-            with open(path, 'r') as file:
-                file.readline()  # Skip header
-                reader = csv.reader(file)
-                for row in reader:
-                    bugs.append(self._to_dict(row))
-        return bugs
+        for path in self._get_files(directory, pattern='bugs.*.json'):
+            bugs = helpers.load_json(path, sanitize=False)
+            for bug in bugs:
+                if id == bug['id']:
+                    return bug
+        raise Exception('No bug identified by {}'.format(id))
+
+    def get_bugs(self, year):
+        """Yield bugs that were published in a specified year.
+
+        Parameters
+        ----------
+        year : int
+            Bugs published in the specified year must be returned.
+
+        Return
+        ------
+        bugs : generator
+            Iterator-like object that allows iteration over the bugs being
+            returned without blocking the caller.
+        """
+        directory = self.get_bugs_path(year)
+        for path in self._get_files(directory, pattern='bugs.*.json'):
+            for bug in helpers.load_json(path):
+                yield bug
 
     def get_bugs_path(self, year):
         """
@@ -271,36 +299,12 @@ class Files(object):
         """
         pass
 
-    # TODO: Remove function once bug information is available in JSON format
-    def _to_dict(self, row):
-        """
-        The monorail API is awful, and our credentials don't work. So this
-        function grabs the CSV file, adds a few keys, and converts everything
-        into json.
-        """
-        bug = dict()
-
-        bug['id'] = row[0]
-        bug['type'] = row[1]
-        bug['cve'] = row[2]
-        bug['status'] = row[3]
-        bug['opened'] = row[4]
-        bug['closed'] = row[6]
-        bug['modified'] = row[8]
-        bug['summary'] = row[10]
-        bug['labels'] = row[11]
-
-        return bug
-
     def _save(self, directory, chunk, items, errors, switch):
         """
         Save the specified reviews to a json file in the reviews path
         associated with the specified year. Format according to the specified
         chunk. Log errors in a CSV file in the same directory.
         """
-        if switch not in ['bugs', 'reviews']:
-            raise ValueError('Argument switch must be \'bugs\' or \'reviews\'')
-
         if not os.path.exists(directory):
             os.mkdir(directory, mode=0o755)
 

app/lib/loaders/bug.py
@@ -22,7 +22,7 @@ def aggregate(oqueue, cqueue, num_doers):
             done += 1
             if done == num_doers:
                 break  # All doers are done
-            continue # pragma: no cover
+            continue  # pragma: no cover
 
         (bug, cves) = item
         try:
@@ -41,7 +41,7 @@ def aggregate(oqueue, cqueue, num_doers):
                 if objects:
                     VulnerabilityBug.objects.bulk_create(objects)
                 count += 1
-        except Error as err: # pragma: no cover
+        except Error as err:  # pragma: no cover
             sys.stderr.write('Exception\n')
             sys.stderr.write('  Bug  {}\n'.format(bug.id))
             extype, exvalue, extrace = sys.exc_info()
@@ -50,25 +50,22 @@ def aggregate(oqueue, cqueue, num_doers):
     oqueue.put(count)
 
 
-def do(iqueue, cqueue): # pragma: no cover
+def do(iqueue, cqueue):  # pragma: no cover
     while True:
         item = iqueue.get()
         if item == parallel.EOI:
             cqueue.put(parallel.DD)
             break
 
+        type, cves = '', list()
+        for label in item['labels']:
+            if label.startswith('Type-'):
+                type = label.replace('Type-', '')
+            if label.startswith('CVE-'):
+                cves.append(label)
         bug = Bug(
-                id=item['id'], type=item['type'], status=item['status'],
-                document=item
+                id=item['id'], type=type, status=item['status'], document=item
             )
-        cves = list()
-        if item['cve'] != '':
-            cves = [
-                    'CVE-{}'.format(cve.strip())
-                    for cve in item['cve'].split(',')
-                ]
-
-        #print("BUG: " + str(bug.id))
         cqueue.put((bug, cves))
 
 

app/tests/lib/loaders/test_bug.py
@@ -32,6 +32,34 @@ class BugLoaderTestCase(test.TestCase):
         actual = list(Bug.objects.all().values_list('id', flat=True))
         self.assertCountEqual(expected, actual, msg='Data: Bug')
 
+        # Bug Type
+        expected = (
+                [   # 2012
+                    (138542, 'Bug')
+                ] +
+                [   # 2015
+                    (492263, 'Bug-Security'), (460986, 'Bug'), (517548, 'Bug'),
+                    (514551, 'Bug-Regression'), (514246, 'Bug-Regression'),
+                    (545318, 'Launch-OWP'), (542060, 'Bug-Security'),
+                    (528486, 'Bug'), (534718, 'Bug'), (522049, 'Bug'),
+                    (521057, 'Bug'), (521826, 'Bug'), (522587, 'Bug'),
+                    (522791, 'Bug-Security')
+                ] +
+                [   # 2016
+                    (620126, 'Bug'), (613918, 'Bug-Security'),
+                    (619379, 'Bug-Security'), (617492, 'Bug'),
+                    (624894, 'Feature'), (626102, 'Bug'), (627655, 'Bug'),
+                    (628110, 'Bug'), (625357, 'Bug'), (636539, 'Bug'),
+                    (576270, 'Bug'), (610176, 'Bug'), (609260, 'Bug-Security'),
+                    (613160, 'Bug-Security'), (602509, 'Bug'), (606056, 'Bug'),
+                    (607690, 'Bug'), (583485, 'Bug'), (584783, 'Bug'),
+                    (642598, 'Bug'), (628496, 'Bug')
+                ]
+            )
+
+        actual = list(Bug.objects.all().values_list('id', 'type'))
+        self.assertCountEqual(expected, actual, msg='Data: Bug Type')
+
         # Vulnerabilities
         expected = (
                 [   # 2015
@@ -59,7 +87,6 @@ class BugLoaderTestCase(test.TestCase):
                     ('CVE-2016-1702', 609260),
                     ('CVE-2016-5167', 613918),
                     ('CVE-2016-5167', 619379),
-                    ('CVE-2016-5167', 626102),
                     ('CVE-2016-5167', 642598)
                 ]
             )

app/tests/management/commands/test_loaddb.py
@@ -146,7 +146,6 @@ class LoaddbTestCase(TransactionTestCase):
                     ('CVE-2016-1702', 609260),
                     ('CVE-2016-5167', 613918),
                     ('CVE-2016-5167', 619379),
-                    ('CVE-2016-5167', 626102),
                     ('CVE-2016-5167', 642598)
                 ] +
                 [   # Manually Added

app/tests/test_files.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import os
 import tempfile
 
@@ -14,12 +15,28 @@ class FilesTestCase(TestCase):
     def setUp(self):
         self.files = files.Files(settings)
 
+    def test_get_bug_w_year(self):
+        actual = self.files.get_bug(id=576270, year=2016)
+
+        self.assertTrue(type(actual) is dict)
+        self.assertEqual(576270, actual['id'])
+
+    def test_get_bug_wo_year(self):
+        actual = self.files.get_bug(id=576270)
+
+        self.assertTrue(type(actual) is dict)
+        self.assertEqual(576270, actual['id'])
+
+    def test_get_bug_nonexistent(self):
+        with self.assertRaises(Exception):
+            _ = self.files.get_bug(id=40758, year=2016)
+
     def test_get_bugs(self):
         expected = [
-                '606056', '610176', '627655', '602509', '584783', '628496',
-                '624894', '617492', '609260', '613160', '625357', '628110',
-                '576270', '620126', '607690', '583485', '636539', '613918',
-                '619379', '626102', '642598'
+                606056, 610176, 627655, 602509, 584783, 628496,
+                624894, 617492, 609260, 613160, 625357, 628110,
+                576270, 620126, 607690, 583485, 636539, 613918,
+                619379, 626102, 642598
             ]
         actual = self.files.get_bugs(year=2016)
 
@@ -114,8 +131,14 @@ class FilesTestCase(TestCase):
 
         self.assertEqual(expected, actual)
 
-    def test_get_review(self):
+    def test_get_review_w_year(self):
         actual = self.files.get_review(id=1999153002, year=2016)
+
+        self.assertTrue(type(actual) is dict)
+        self.assertEqual(1999153002, actual['issue'])
+
+    def test_get_review_wo_year(self):
+        actual = self.files.get_review(id=1999153002)
 
         self.assertTrue(type(actual) is dict)
         self.assertEqual(1999153002, actual['issue'])
@@ -172,15 +195,44 @@ class FilesTestCase(TestCase):
         with self.assertRaises(Exception):
             _ = self.files.get_year(id=40758, switch='bugs')
 
+    def test_save(self):
+        data = {
+                'data': [{'id': id} for id in range(1, 10)],
+                'errors': list(range(1, 10))
+            }
+        expected = data
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            directory = os.path.join(tempdir, 'foo')
+            self.files._save(directory, 7, data['data'], data['errors'], 'bar')
+
+            self.assertTrue(os.path.exists(directory))
+
+            actual = dict()
+
+            path = os.path.join(directory, 'bar.7.json')
+            self.assertTrue(os.path.exists(path))
+            with open(path) as file:
+                actual['data'] = json.load(file)
+
+            path = os.path.join(directory, 'errors.csv')
+            self.assertTrue(os.path.exists(path))
+            with open(path) as file:
+                actual['errors'] = list()
+                reader = csv.reader(file)
+                for row in reader:
+                    actual['errors'].append(int(row[0]))
+            self.assertCountEqual(expected, actual)
+
     def test_save_ids(self):
         data = list(range(100001, 100010))
         expected = [str(item) for item in data]
 
         with tempfile.TemporaryDirectory() as tempdir:
-            with self.settings(IDS_PATH=os.path.join('/tmp', tempdir)):
+            with self.settings(IDS_PATH=tempdir):
                 f = files.Files(settings)
                 f.save_ids(year=9999, ids=data, switch='reviews')
-                path = os.path.join('/tmp', tempdir, '9999.csv')
+                path = os.path.join(tempdir, '9999.csv')
 
                 self.assertTrue(os.path.exists(path))
                 actual = None
@@ -189,19 +241,33 @@ class FilesTestCase(TestCase):
                     actual = [row[0] for row in reader]
                 self.assertCountEqual(expected, actual)
 
+    def test_save_bugs(self):
+        data = [{'id': id} for id in range(100001, 100010)]
+        expected = data
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            with self.settings(BUGS_PATH=os.path.join(tempdir, '{year}')):
+                f = files.Files(settings)
+                f.save_bugs(year=9999, chunk=1, bugs=data, errors=None)
+                path = os.path.join(tempdir, '9999')
+                self.assertTrue(os.path.exists(path))
+                path = os.path.join(tempdir, '9999', 'bugs.1.json')
+                self.assertTrue(os.path.exists(path))
+
+                actual = f.get_bugs(year=9999)
+                self.assertCountEqual(expected, actual)
+
     def test_save_reviews(self):
         data = [{'issue': id} for id in range(100001, 100010)]
         expected = data
 
         with tempfile.TemporaryDirectory() as tempdir:
-            with self.settings(
-                    REVIEWS_PATH=os.path.join('/tmp', tempdir, '{year}')
-                 ):
+            with self.settings(REVIEWS_PATH=os.path.join(tempdir, '{year}')):
                 f = files.Files(settings)
                 f.save_reviews(year=9999, chunk=1, reviews=data, errors=None)
-                path = os.path.join('/tmp', tempdir, '9999')
+                path = os.path.join(tempdir, '9999')
                 self.assertTrue(os.path.exists(path))
-                path = os.path.join('/tmp', tempdir, '9999', 'reviews.1.json')
+                path = os.path.join(tempdir, '9999', 'reviews.1.json')
                 self.assertTrue(os.path.exists(path))
 
                 actual = f.get_reviews(year=9999)

commit d8b2bbeb90b199e9426f5e3adfa7bdd7cdcb42fc

Fix bug introduced in e9c70f9b
The assumption made when loading bugs in their JSON format was to not
sanitize unicode null characters in the raw JSON text before attempting
to load and save the JSON to the database. The assumption, however, was
shown to be incorrect when a load was attempted. The change fixes the
bug that was caused by the incorrect assumption.

app/lib/files.py
@@ -42,7 +42,7 @@ class Files(object):
         year = self.get_year(id, switch='bugs') if year is None else year
         directory = self.get_bugs_path(year)
         for path in self._get_files(directory, pattern='bugs.*.json'):
-            bugs = helpers.load_json(path, sanitize=False)
+            bugs = helpers.load_json(path)
             for bug in bugs:
                 if id == bug['id']:
                     return bug