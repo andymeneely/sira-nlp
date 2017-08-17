b3bce288797795708f0b18328ee4ea0baeecafa2
Nuthan Munaiah <nuthan.munaiah@live.com>
2017-08-17 01:14:06 -0400

    Use Chi-squared to test for independence
    The change adds tests to check for independence of comment type with
    uncertainty of comment.

--- app/lib/analysis/tests/association/comment.R
+++ app/lib/analysis/tests/association/comment.R
@@ -4,7 +4,7 @@ source("data/comment.R")
 
 InitGlobals()
 
-## Test: Continuous-values Metrics ====
+## Test: Continuous-valued Metrics ====
 metrics <- COMMENT.CV.METRICS
 
 test.outcomes <- data.frame()
@@ -31,6 +31,38 @@ for (i in 1:length(metrics)) {
     SaveSplits(splits, granularity = "comment", metric = metric.variant)
 
     test.outcome <- GetAssociation(splits$x, splits$y, "useful", "notuseful")
+    test.outcome <- data.frame("metric" = variant.label, test.outcome)
+    rownames(test.outcome) <- c()
+    test.outcomes <- rbind(test.outcomes, test.outcome)
+  }
+}
+print(test.outcomes)
+
+## Test: Boolean-valued Metrics ====
+metrics <- COMMENT.BV.METRICS
+
+test.outcomes <- data.frame()
+for (i in 1:length(metrics)) {
+  metric <- metrics[i]
+  metric.label <- COMMENT.METRIC.LABELS[[metric]]
+  cat("[", i, "/", length(metrics), "] ", metric.label, "\n", sep = "")
+
+  # dataset <- GetCommentMetric(metric)
+  # analysis.dataset <- dataset %>%
+  #   inner_join(., COMMENT.TYPE, by = "comment_id") %>%
+  #   select(-comment_id)
+
+  variants <- c(metric)
+  if (metric %in% names(COMMENT.BV.METRIC.VARIANTS)) {
+    variants <- unlist(COMMENT.BV.METRIC.VARIANTS[[metric]])
+  }
+  for (j in 1:length(variants)) {
+    metric.variant <- variants[j]
+    variant.label <- COMMENT.METRIC.LABELS[[metric.variant]]
+    cat(" [", j, "/", length(variants), "] ", variant.label, "\n", sep = "")
+
+    test.outcome <- GetIndependence(analysis.dataset$type,
+                                    analysis.dataset[[metric.variant]])
     test.outcome <- data.frame("metric" = variant.label, test.outcome)
     rownames(test.outcome) <- c()
     test.outcomes <- rbind(test.outcomes, test.outcome)
