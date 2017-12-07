3c6390d0a6311e840c25ef46a3f0ee623e1006c3
Nuthan Munaiah <nuthan.munaiah@live.com>
2017-12-07 03:03:50 -0500

    Read useful and not useful comments from file

--- app/lib/analysis/data/data.R
+++ app/lib/analysis/data/data.R
@@ -1,25 +1,33 @@
 # Data Source: Database ----
 
 GetCommentType <- function(connection, normalize = FALSE){
-  notuseful.comments <- GetNotUsefulComments()
-  if (nrow(notuseful.comments) > 0) {
+  useful.comments <- GetUsefulComments()
+  if (nrow(useful.comments) == 0) {
     query <- "
       SELECT c.id AS comment_id,
         'useful'::text AS type
       FROM comment c
       WHERE c.by_reviewer IS true AND c.is_useful IS true;
     "
-    dataset <- GetData(connection, query) %>%
-      bind_rows(notuseful.comments)
-  } else {
+    useful.comments <- GetData(connection, query)
+  }
+  useful.comments$type <- factor(useful.comments$type,
+                                 levels = c("notuseful", "useful"))
+
+  notuseful.comments <- GetNotUsefulComments()
+  if (nrow(notuseful.comments) == 0) {
     query <- "
       SELECT c.id AS comment_id,
-        CASE WHEN is_useful IS true THEN 'useful' ELSE 'notuseful' END AS type
+        'notuseful'::text AS type
       FROM comment c
-      WHERE c.by_reviewer IS true;
+      WHERE c.by_reviewer IS true AND c.is_useful IS false;
     "
-    dataset <- GetData(connection, query)
+    notuseful.comments <- GetData(connection, query)
   }
+  notuseful.comments$type <- factor(notuseful.comments$type,
+                                    levels = c("notuseful", "useful"))
+
+  dataset <- bind_rows(useful.comments, notuseful.comments)
   dataset$type <- factor(dataset$type, levels = c("notuseful", "useful"))
   return(dataset)
 }
@@ -29,5 +37,11 @@ GetCommentType <- function(connection, normalize = FALSE){
 GetNotUsefulComments <- function() {
   if (file.exists("data/notuseful.csv"))
     return(read.csv("data/notuseful.csv", header = T))
+  return(data.frame())
+}
+
+GetUsefulComments <- function() {
+  if (file.exists("data/useful.csv"))
+    return(read.csv("data/useful.csv", header = T))
   return(data.frame())
 }
